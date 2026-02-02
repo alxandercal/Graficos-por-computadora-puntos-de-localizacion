397092ec67d556ee6e889540807a451fdd703a94
Alejandro Villaseñor <alefeco2003@gmail.com>
2026-02-01 19:49:16 -0600

    [UPDATED]: Se hizo que en el codigo de lps puntos mas cercanos el usuario asignara los cajeros .

--- Punto_mas_cercano.py
+++ Punto_mas_cercano.py
@@ -9,19 +9,29 @@ def calc_dist(cajeros,origen):
 
 def dist_mas_cerca(origen,cajeros):#el origen es la coordenada del usuario
     mas_cerca= 999999999
-    for n in range(10):
+    for n in range(len(cajeros)):
         dist_actual=calc_dist(cajeros[n],origen)
         if (dist_actual < mas_cerca):
             poss=n
             mas_cerca=dist_actual
     return poss,mas_cerca
 
+def Crea_cajeros():
+    poligono = []
+    vertices=int(input("Cuantos Cajeros vas a crear? "))
+    for n in range(vertices):
+        entrada = input(f"Cajero {n + 1}: - Ingresa X y Y separados por coma: ")
+        coordenadas = tuple(int(i.strip()) for i in entrada.split(","))
+        poligono.append(coordenadas)
+    return poligono
+
 def imprime_cajeros(cajeros):
     print("Los cajeros disponibles en la region son los siguientes:")
-    for n in range(10):
-        print("Cajero",n+1 ,cajeros[n])
+    for n in range(len(cajeros)):
+        print(f"Cajero{n+1}: {cajeros[n]} " )
+
 def main():
-    cajeros=( (10,10),(50,10),(90,10),(10,50),(50,50),(90,50),(10,90),(50,90),(80,80),(90,90)   )
+    cajeros=Crea_cajeros()
     imprime_cajeros(cajeros)
     entrada = input("Ingresa las coordenadas X y Y separadas por una coma: ")
     usuario = [int(n) for n in entrada.split(",")]
